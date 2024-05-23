e175ae98ce44dddea1ea1f7ae2d09179b2f2b2e1
Mwaiwathu Laurent Chatha <151863592+MwaiwathuChatha@users.noreply.github.com>
2024-05-23 21:56:09 +0200

    More changes to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import {resolve} from "path";
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    build:{
-        rollupOptions:{
-            input:{
+  build: {
+    rollupOptions: {
+      input: {
                 main:resolve(__dirname,"index.html"),
-                food_category_page:resolve(__dirname,"/pages/food-category-page/index.html"),
-                letter_search_page:resolve(__dirname,"/pages/letter-search-page/index.html"),
-                random_meal_page:resolve(__dirname,"/pages/random-meal-page/index.html"),
-                search_meal_page:resolve(__dirname,"/pages/search-meal-page/index.html")
+                food_category_page:resolve(__dirname,"pages/food-category-page/index.html"),
+                letter_search_page:resolve(__dirname,"pages/letter-search-page/index.html"),
+                random_meal_page:resolve(__dirname,"pages/random-meal-page/index.html"),
+                search_meal_page:resolve(__dirname,"pages/search-meal-page/index.html"),
             }
         }
     }
